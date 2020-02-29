59c0e789d30a08858bcd96d4818d279752cb9b09
Max <osinov.maks@gmail.com>
2020-02-29 21:24:12 +0200

    create computer controller

---
+++ cloudSite/cloudSite/Controllers/ComputerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cloudSite.Controllers
+{
+    public class ComputerController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
