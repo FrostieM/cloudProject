262c0376c03352ef432f6a37a280268031d60ba5
Max <osinov.maks@gmail.com>
2020-03-01 15:05:21 +0200

    update site part
    add view

--- cloudSite/cloudSite/Controllers/ComputerController.cs
+++ cloudSite/cloudSite/Controllers/ComputerController.cs
@@ -1,13 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections;
+using cloudSite.ViewData;
+using Microsoft.AspNetCore.Mvc;
 
 namespace cloudSite.Controllers
 {
-    public class ComputerController : Controller
+    [Route("api/[controller]")]
+    public class ComputerController : ControllerBase
     {
-        // GET
-        public IActionResult Index()
+    
+        [HttpGet]
+        public IEnumerable GetComputers()
         {
-            return View();
+            return new[]
+            {
+                new ComputerViewData{ Name = "123.214.152.215", Programs = 5},
+                new ComputerViewData{ Name = "325.23.235.234", Programs = 2},
+                new ComputerViewData{ Name = "235.235.234.532", Programs = 3},
+                new ComputerViewData{ Name = "234.425.65.434", Programs = 10},
+                new ComputerViewData{ Name = "132.12.422.24", Programs = 12},
+                new ComputerViewData{ Name = "143.53.24.42", Programs = 42},
+            };
+        }
+        
+        [Route("computerInfo")]
+        [HttpGet]
+        public IEnumerable GetComputerInfo(string computerName)
+        {
+            Console.WriteLine(computerName);
+            return new[] { "cs go", "dota", "fortnite", "dauntless", "vs code", "firefox" };
         }
     }
 }
---
+++ cloudSite/cloudSite/ViewData/ComputerViewData.cs
@@ -0,0 +1,8 @@
+﻿namespace cloudSite.ViewData
+{
+    public class ComputerViewData
+    {
+        public string Name { get; set; }
+        public int Programs { get; set; }
+    }
+}
